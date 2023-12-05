adee15705e91cc9ca3009afb49700b905867ec0e
Nate Szelag <nszelag@getmatterhorn.com>
2023-12-04 20:54:04 -0500

    Day1 Part 2

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -4,32 +4,26 @@ const fileContent = fs.readFileSync(dataFilePath, 'utf-8');
 
 let sum = 0;
 
+const numbers = {
+	one: 'on1e',
+	two: 'tw2o',
+	three: 'thr3ee',
+	four: 'fou4r',
+	five: 'fi5ve',
+	six: 'si6x',
+	seven: 'se7ven',
+	eight: 'ei8ght',
+	nine: 'ni9ne',
+};
+
 fileContent.split(/\r?\n/).forEach((line) => {
-	const lineChars = line.split('');
-
-	let firstDigit;
-	let secondDigit;
-
-	for (let i = 0; i < lineChars.length; i++) {
-		if (!isNaN(lineChars[i]) && !firstDigit) {
-			firstDigit = lineChars[i];
-		}
-
-		const lastIndex = lineChars.length - 1 - i;
-		if (!isNaN(lineChars[lastIndex]) && !secondDigit) {
-			secondDigit = lineChars[lastIndex];
-		}
-
-		if (firstDigit && secondDigit) {
-			break;
-		}
+	for (const num in numbers) {
+		const regex = new RegExp(num, 'g');
+		line = line.replace(regex, `${numbers[num]}`);
 	}
 
-	if (!secondDigit) {
-		secondDigit = firstDigit;
-	}
-
-	sum += parseInt(firstDigit + secondDigit, 10);
+	const match = line.match(/\d/g);
+	sum += parseInt(match[0] + match[match.length - 1]);
 });
 
 console.log(sum);
